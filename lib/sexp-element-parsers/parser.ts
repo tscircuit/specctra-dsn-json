d86411683d6982aeb43c80bdbc513ee021ef6972
Andrii Balitskyi <10balian10@gmail.com>
2024-07-01 10:40:32 +0200

    Add new cases to parseSexprParser

--- lib/sexp-element-parsers/parser.ts
+++ lib/sexp-element-parsers/parser.ts
@@ -4,12 +4,25 @@ export function parseSexprParser(element: any[]): Parser {
   const parserElement: Parser = {}
 
   element.forEach((e) => {
-    if (e[0] === "space_in_quoted_tokens") {
-      parserElement.space_in_quoted_tokens = e[1]
-    } else if (e[0] === "host_cad") {
-      parserElement.host_cad = e[1]
-    } else if (e[0] === "host_version") {
-      parserElement.host_version = e[1]
+    switch (e[0]) {
+      case "space_in_quoted_tokens":
+        parserElement.space_in_quoted_tokens = e[1] === "on"
+        break
+      case "host_cad":
+        parserElement.host_cad = e[1]
+        break
+      case "host_version":
+        parserElement.host_version = e[1]
+        break
+      case "constant":
+        if (!parserElement.constant) {
+          parserElement.constant = {}
+        }
+        parserElement.constant[e[1][0]] = e[1][1]
+        break
+      case "generated_by_freeroute":
+        parserElement.generated_by_freeroute = true
+        break
     }
   })
 
