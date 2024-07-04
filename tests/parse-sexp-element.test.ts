5a5ec8f2d8330e9581273bf8669a46e41a4426c8
Andrii Balitskyi <10balian10@gmail.com>
2024-07-04 15:34:44 +0200

    Update sample asset, update test

--- tests/parse-sexp-element.test.ts
+++ tests/parse-sexp-element.test.ts
@@ -16,4 +16,5 @@ test("parseSexpElement parses s-expression array representation without throwing
   t.truthy(result.unit)
   t.truthy(result.structure)
   t.truthy(result.placement)
+  t.truthy(result.library)
 })
