5df1fc315d437df27bee321458c472efb626a55d
Andrii Balitskyi <10balian10@gmail.com>
2024-07-04 15:32:37 +0200

    Break out parseKeepout into a separate file, add parseShape

---
+++ lib/sexp-to-json-interpreters/keepout.ts
@@ -0,0 +1,23 @@
+import type { Keepout } from "lib/types"
+import { keepoutSchema } from "lib/zod-schema"
+import { SHAPE_NAMES, parseShape } from "./shape"
+
+export function parseKeepout(value: any[]): Keepout {
+  const keepoutObj: Partial<Keepout> = { type: "keepout" }
+
+  if (typeof value[0] === "string") {
+    keepoutObj.id = value[0]
+    value = value.slice(1)
+  }
+
+  value.forEach((v: any) => {
+    const [key, ...rest] = v
+    if (SHAPE_NAMES.has(key)) {
+      keepoutObj.shape = parseShape([key, ...rest])
+    } else if (key === "clearance_class") {
+      keepoutObj.clearance_class = rest[0]
+    }
+  })
+
+  return keepoutSchema.parse(keepoutObj)
+}
