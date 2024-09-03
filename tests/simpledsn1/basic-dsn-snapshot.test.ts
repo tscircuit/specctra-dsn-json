c67b041134ee7193697245a7d29c532533713bcb
seveibar <seveibar@gmail.com>
2024-09-03 11:29:12 -0700

    wip

--- tests/simpledsn1/basic-dsn-snapshot.test.ts
+++ tests/simpledsn1/basic-dsn-snapshot.test.ts
@@ -4,9 +4,13 @@ import { circuitJsonToPcbSvg } from "circuit-to-svg"
 import simpledsn1 from "./simpledsn1.dsn" with { "type": "text" }
 import { parseDsnToJson } from "lib"
 import { convertDsnJsonToTscircuitSoupJson } from "pcb-json-to-tscircuit-soup-json"
+import { writeFileSync } from "node:fs"
 
 test("generate soup and match SVG snapshot", () => {
   const dsnJson = parseDsnToJson(simpledsn1)
+
+  // writeFileSync("simpledsn1.json", JSON.stringify(dsnJson, null, 2))
+
   const soupElements = convertDsnJsonToTscircuitSoupJson(dsnJson)
 
   const svg = circuitJsonToPcbSvg(soupElements as any)
