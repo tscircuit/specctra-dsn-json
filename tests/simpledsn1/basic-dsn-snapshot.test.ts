561eb72e12996c63f75bf89dbefd7bcac0a5774c
seveibar <seveibar@gmail.com>
2024-09-03 11:43:41 -0700

    building smtpads

--- tests/simpledsn1/basic-dsn-snapshot.test.ts
+++ tests/simpledsn1/basic-dsn-snapshot.test.ts
@@ -6,6 +6,7 @@ import { parseDsnToJson } from "lib"
 import { writeFileSync } from "node:fs"
 import type { DsnPcbDesign } from "lib/types"
 import type { PCBSMTPad } from "@tscircuit/soup"
+import { logSoup } from "@tscircuit/log-soup"
 
 function convertDsnJsonToTscircuitSoupJson(dsnJson: DsnPcbDesign): Array<{
   x: number
@@ -17,16 +18,7 @@ function convertDsnJsonToTscircuitSoupJson(dsnJson: DsnPcbDesign): Array<{
   width: number // in mm
   height: number // in mm
 }> {
-  const soupElements: Array<{
-    x: number
-    y: number
-    type: "pcb_smtpad"
-    layer: "top" | "bottom"
-    shape: "rect"
-    pcb_smtpad_id: string
-    width: number
-    height: number
-  }> = []
+  const soupElements: Array<PCBSMTPad> = []
 
   dsnJson.placement.forEach((component) => {
     component.places.forEach((place) => {
@@ -39,23 +31,25 @@ function convertDsnJsonToTscircuitSoupJson(dsnJson: DsnPcbDesign): Array<{
             (lib) => "padstack" in lib && lib.padstack.name === pin.name,
           )
           if (padstack && "padstack" in padstack) {
-            const shape = padstack.padstack.shapes[0]
+            const shape = padstack.padstack.shapes[0]!
             if (shape.type === "polygon") {
               const xCoords = shape.coordinates.map((coord) => coord[0])
               const yCoords = shape.coordinates.map((coord) => coord[1])
               const width = Math.max(...xCoords) - Math.min(...xCoords)
               const height = Math.max(...yCoords) - Math.min(...yCoords)
 
-              soupElements.push({
-                x: place.x + pin.x,
-                y: place.y + pin.y,
+              const pcbSmtpad: PCBSMTPad = {
+                x: (place.x + pin.x) / 1000, //um -> mm
+                y: (place.y + pin.y) / 1000, //um -> mm
                 type: "pcb_smtpad",
                 layer: place.side === "front" ? "top" : "bottom",
                 shape: "rect",
                 pcb_smtpad_id: `${place.component_id}-${index + 1}`,
                 width: width / 1000, // Convert um to mm
                 height: height / 1000, // Convert um to mm
-              })
+              }
+
+              soupElements.push(pcbSmtpad)
             }
           }
         })
@@ -66,12 +60,14 @@ function convertDsnJsonToTscircuitSoupJson(dsnJson: DsnPcbDesign): Array<{
   return soupElements
 }
 
-test("generate soup and match SVG snapshot", () => {
+test("generate soup and match SVG snapshot", async () => {
   const dsnJson = parseDsnToJson(simpledsn1)
 
   // writeFileSync("simpledsn1.json", JSON.stringify(dsnJson, null, 2))
 
   const soupElements = convertDsnJsonToTscircuitSoupJson(dsnJson)
+
+  await logSoup("simpledsn1", soupElements)
 
   const svg = circuitJsonToPcbSvg(soupElements as any)
   expect(svg).toMatchSvgSnapshot(import.meta.path)
