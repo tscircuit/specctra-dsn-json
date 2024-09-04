5d86c1b68190dce44ef7fffc58c5a981211c4fab
Andrii Balitskyi <10balian10@gmail.com>
2024-09-04 18:27:44 +0200

    Adjust to the update pad parsing, convert round rect to rect

--- pcb-json-to-tscircuit-soup-json/index.ts
+++ pcb-json-to-tscircuit-soup-json/index.ts
@@ -2,19 +2,15 @@ import type { PathShape, DsnPcbDesign } from "lib/types"
 import * as Soup from "@tscircuit/soup"
 import type { AnySoupElement } from "@tscircuit/soup"
 import { mm } from "@tscircuit/mm"
-import {
-  extractLayerFromPinName,
-  isPlatedHole,
-  isSmtPad,
-  parsePinName,
-} from "./parse-pin-name"
+import { isPlatedHole, isSmtPad, parsePadName } from "./parse-pad-name"
 
 export const convertDsnJsonToTscircuitSoupJson = (
-  pcb: DsnPcbDesign,
+  pcb: DsnPcbDesign
 ): AnySoupElement[] => {
   const soupElements: AnySoupElement[] = []
 
   const pcbComponents = pcb.placement.flatMap((component) => component.places)
+  const pcbImages = pcb.library.filter((el) => "image" in el)
 
   for (const [
     index,
@@ -31,22 +27,21 @@ export const convertDsnJsonToTscircuitSoupJson = (
     })
 
     const pcbComponentName = pcb.placement.find((p) =>
-      p.places.some((p) => p.component_id === component_id),
+      p.places.some((p) => p.component_id === component_id)
     )?.component
 
-    const pcbImages = pcb.library.filter((el) => "image" in el)
     const pcbComponentImage = pcbImages.find(
-      (el) => el.image.name === pcbComponentName,
+      (el) => el.image.name === pcbComponentName
     )?.image
     if (!pcbComponentImage) {
       throw new Error(`PCB component image not found: ${pcbComponentName}`)
     }
 
     const pcbComponentOutlines = pcbComponentImage.outlines.filter((el) =>
-      el.type.includes("path"),
+      el.type.includes("path")
     ) as PathShape[]
     const pcbComponentDimensions = calculatePcbComponentDimensions(
-      pcbComponentOutlines,
+      pcbComponentOutlines
     ) ?? { width: 0, height: 0 }
 
     const soupPcbComponent = Soup.pcb_component.parse({
@@ -65,14 +60,14 @@ export const convertDsnJsonToTscircuitSoupJson = (
     componentSoupElements.push(soupSourceComponent, soupPcbComponent)
 
     pcbComponentImage.pins.sort(
-      (a, b) => Number(a.pin_number) - Number(b.pin_number),
+      (a, b) => Number(a.pin_number) - Number(b.pin_number)
     )
 
     const pcbComponentSmtPads = pcbComponentImage.pins.filter(({ name }) =>
-      isSmtPad(name),
+      isSmtPad(name)
     )
     for (const { pin_number, x, y, name } of pcbComponentSmtPads) {
-      const { shape, layer, width, height, radius } = parsePinName(name)
+      const { shape, layer, width, height, diameter } = parsePadName(name)
       const soupSourcePort = Soup.source_port.parse({
         type: "source_port",
         source_port_id: `source_port_${pin_number}`,
@@ -80,27 +75,65 @@ export const convertDsnJsonToTscircuitSoupJson = (
         name: pin_number,
       })
 
-      const pad = {
+      const padShapeMap = {
+        Round: "circle",
+        Oval: "circle",
+        RoundRect: "rect",
+        Rect: "rect",
+        Cust: "rect",
+      }
+      const padLayerMap = {
+        A: "top",
+        T: "top",
+        B: "bottom",
+      }
+
+      const smtPad = {
         pcb_smtpad_id: `pcb_smtpad_${pin_number}`,
         pcb_component_id: soupPcbComponent.pcb_component_id,
         pcb_port_id: soupSourcePort.source_port_id,
         type: "pcb_smtpad",
         x: mm(x),
         y: mm(y),
-        shape,
-        layer,
+        shape: padShapeMap[shape as keyof typeof padShapeMap],
+        layer: padLayerMap[layer as keyof typeof padLayerMap],
         width,
         height,
-        ...(radius ? { radius } : {}),
+        ...(diameter && { radius: diameter / 2 }),
       }
-      const soupPcbPin = Soup.pcb_smtpad.parse(pad)
+
+      if (shape === "RoundRect") {
+        // Transforming round rect pad into rect because tscircuit json only supports rect
+        const padShape = pcb.library
+          .filter((el) => "padstack" in el)
+          .find((p) => p.padstack.name === name)?.padstack.shapes[0]
+
+        if (!padShape) {
+          throw new Error(`${name} pad shape not found in the pcb dsn library`)
+        }
+
+        if (padShape.type !== "polygon") {
+          throw new Error(
+            `Invalid pad shape type ${padShape.type} for round rect pad. Expected polygon`
+          )
+        }
+
+        const { width, height } = roundRectCoordinatesToRect(
+          padShape.coordinates
+        )
+
+        smtPad.width = width
+        smtPad.height = height
+      }
+
+      const soupPcbPin = Soup.pcb_smtpad.parse(smtPad)
 
       componentSoupElements.push(soupSourcePort, soupPcbPin)
       soupElements.push(...componentSoupElements)
     }
 
     const pcbComponentPlatedHoles = pcbComponentImage.pins.filter(({ name }) =>
-      isPlatedHole(name),
+      isPlatedHole(name)
     )
   }
 
@@ -127,6 +160,15 @@ function calculatePcbComponentDimensions(outlines: PathShape[]) {
 
   const width = maxX - minX
   const height = maxY - minY
+
+  return { width: mm(width), height: mm(height) }
+}
+
+function roundRectCoordinatesToRect(roundRectCoords: [number, number][]) {
+  const xCoords = roundRectCoords.map((coord) => coord[0])
+  const yCoords = roundRectCoords.map((coord) => coord[1])
+  const width = Math.max(...xCoords) - Math.min(...xCoords)
+  const height = Math.max(...yCoords) - Math.min(...yCoords)
 
   return { width: mm(width), height: mm(height) }
 }
